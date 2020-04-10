f333b0ffda3f4ff58775770b7ed1c276f701c3e8
diogomotapinto <diogomotapinto@gmail.com>
2020-04-10 18:43:52 +0100

    hot fix

--- MyUser/User.cs
+++ MyUser/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-namespace Server
+namespace MyUser
 {
     [Serializable()]
 
--- Server/Logged.cs
+++ Server/Logged.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Server
 {
-    class Class1
+    class Logged
     {
     }
 }
